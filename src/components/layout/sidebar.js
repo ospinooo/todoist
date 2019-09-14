e5c86cfbfea4c8f5f08a432e917400220baa8efa
Ospino <ospino.p@gmail.com>
2019-09-14 12:25:04 +0200

    Use projects in the sidebar ul at the bottom, showProjects state
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/components/layout/sidebar.js
+++ src/components/layout/sidebar.js
@@ -1,33 +1,50 @@
-import React from 'react';
-import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons/fa';
-// Arquitecture
-// b = block
-// e = element
-// m = modifier
+import React, { useState } from 'react';
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar
+} from 'react-icons/fa';
 
+import { useSelectedProjectValue } from '../../context';
+
+import { Projects } from '../Projects';
+import { AddPRoject } from '../AddProject';
 /**
  * Application Sidebar.
  */
 export const Sidebar = () => {
 
+  const { setSelectedProject } = useSelectedProjectValue();
+  const [active, setActive] = useState('inbox');
+  const [showProjects, setShowProjects] = useState(true);
 
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
-        <li><span><FaInbox></FaInbox></span><span>Inbox</span></li>
-        <li><span><FaRegCalendar></FaRegCalendar></span><span>Today</span></li>
-        <li><span><FaRegCalendarAlt></FaRegCalendarAlt></span><span>Next 7 day</span></li>
+        <li data-testid="inbox" className="inbox">
+          <span><FaInbox></FaInbox></span>
+          <span>Inbox</span>
+        </li>
+        <li data-testid="today" className="today">
+          <span><FaRegCalendar></FaRegCalendar></span>
+          <span>Today</span>
+        </li>
+        <li data-testid="next_7" className="next_7">
+          <span><FaRegCalendarAlt></FaRegCalendarAlt></span>
+          <span>Next 7 day</span>
+        </li>
       </ul>
 
-
-      <div className="sidebar__middle">
+      <div className="sidebar__middle" onClick={() => setShowProjects(!showProjects)}>
         <span>
           <FaChevronDown></FaChevronDown>
         </span>
+
         <h2>Projects</h2>
       </div>
-      <ul className="sidebar__projects">Projects will be here!</ul>
-      Add Project Component Here!!
-  </div>
+      <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
+      {showProjects && <AddPRoject />}
+    </div>
   )
 }
