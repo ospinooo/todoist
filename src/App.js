9f714973430745cf9925b17969018ee53fa9467d
Ospino <ospino.p@gmail.com>
2019-09-14 12:14:30 +0200

    Add the context to the app. Wrap the app
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Header } from './components/layout/header';
 import { Content } from './components/layout/content';
+import { ProjectsProvider, SelectedProjectProvider } from './context';
 
+
+import './App.scss'
 // Name exports
-export const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <Header />
-      <Content />
-    </header>
-  </div>
-);
+export const App = () => {
+  return (
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <div className="App">
+          <Header />
+          <Content />
+        </div>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
+  )
+};
 
