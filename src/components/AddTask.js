96fcde4ecaaa1c0e786fc1bc28034f7bbb9084cd
Ospino <ospino.p@gmail.com>
2019-09-15 18:05:12 +0200

    Add task into the list of tasks at the end
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,6 +3,9 @@ import { FaRegListAlt, FaRegCalendarAlt } from 'react-icons/fa';
 import moment from 'moment';
 import { firebase } from '../firebase';
 import { useSelectedProjectValue } from '../context';
+import { ProjectOverlay } from "./ProjectOverlay";
+import { TaskDate } from "./TaskDate";
+
 
 export const AddTask = ({
   showAddTaskMain = true, // main task
@@ -20,6 +23,8 @@ export const AddTask = ({
 
   const { selectedProject } = useSelectedProjectValue();
   const addTask = () => {
+    // project is the selected  by the project overlay. 
+    // selectedProject is the one at the top of the list. 
     const projectId = project || selectedProject;
     let collatedDate = '';
     if (projectId === 'TODAY') {
@@ -50,7 +55,7 @@ export const AddTask = ({
         })
     )
   }
-
+  console.log((showMain || showQuickAddTask));
   return (
     <div
       className={showQuickAddTask ? 'add-task add-task__overlay' : 'add-task'}
@@ -87,50 +92,64 @@ export const AddTask = ({
               </div>
             </>
           )}
-        </div>
-      )}
-      <p>Project Overlay here</p>
-      <p>TaskDate here</p>
-      <input
-        className="add-task__content"
-        data-testid="add-task-content"
-        type="text"
-        value={task}
-        onChange={e => e.target.value}
-      />
-      <button
-        type="button"
-        className="add-task__submit"
-        data-testid="add-task"
-        onClick={() => addTask()}
-      >
-        AddTask
-      </button>
-      {!showQuickAddTask && (
-        <span className="add-task__cancel"
-          data-testid="add-task-main-cancel"
-          onClick={() => {
-            setShowMain(false);
-            setShowProjectOverlay(false);
-          }}
-        >
-          Cancel
+
+          <ProjectOverlay
+            setProject={setProject}
+            showProjectOverlay={showProjectOverlay}
+            setShowProjectOverlay={setShowProjectOverlay} />
+
+          <TaskDate
+            setTaskDate={setTaskDate}
+            showTaskDate={showTaskDate}
+            setShowTaskDate={setShowTaskDate} />
+
+          <input
+            className="add-task__content"
+            data-testid="add-task-content"
+            type="text"
+            value={task}
+            onChange={e => setTask(e.target.value)} // set Task data
+          />
+          <button
+            type="button"
+            className="add-task__submit"
+            data-testid="add-task"
+            onClick={() => showQuickAddTask
+              ? addTask() && setShowQuickAddTask(false)
+              : addTask()}>
+            AddTask
+          </button>
+
+          {!showQuickAddTask && (
+            <span className="add-task__cancel"
+              data-testid="add-task-main-cancel"
+              onClick={() => {
+                setShowMain(false);
+                setShowProjectOverlay(false);
+              }}
+            >
+              Cancel
         </span>
-      )}
-      <span
-        className="add-task__project"
-        data-test-id="show-project-overlay"
-        onClick={() => setShowProjectOverlay(!showProjectOverlay)}
-      >
-        <FaRegListAlt></FaRegListAlt>
-      </span>
-      <span
-        className="add-task__date"
-        data-testid="show-task-date-overlay"
-        onClick={() => setShowTaskDate(!showTaskDate)}
-      >
-        <FaRegCalendarAlt></FaRegCalendarAlt>
-      </span>
-    </div>
+          )}
+
+          {/* OVerlay buttons */}
+          <span
+            className="add-task__project"
+            data-test-id="show-project-overlay"
+            onClick={() => setShowProjectOverlay(!showProjectOverlay)}
+          >
+            <FaRegListAlt></FaRegListAlt>
+          </span>
+          <span
+            className="add-task__date"
+            data-testid="show-task-date-overlay"
+            onClick={() => setShowTaskDate(!showTaskDate)}
+          >
+            <FaRegCalendarAlt></FaRegCalendarAlt>
+          </span>
+        </div>
+      )
+      }
+    </div >
   );
 }
