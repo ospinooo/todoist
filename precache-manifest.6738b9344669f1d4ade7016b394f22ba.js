b242d8657d9f77d9ae7455a8fc254869da8e444a
Ospino <pablo.ospino@nispera.com>
2021-02-19 10:10:17 +0100

    Updates

---
+++ precache-manifest.6738b9344669f1d4ade7016b394f22ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90e85f3655c045fe61c300325443fec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44bc548fac2db6015793",
+    "url": "/static/css/main.e8af1acc.chunk.css"
+  },
+  {
+    "revision": "7bd619225890bcfa758a",
+    "url": "/static/js/2.269979ac.chunk.js"
+  },
+  {
+    "revision": "44bc548fac2db6015793",
+    "url": "/static/js/main.697d7a38.chunk.js"
+  },
+  {
+    "revision": "69875d477f96190fc2fe",
+    "url": "/static/js/runtime~main.7d1fc9c3.js"
+  }
+]);
